cdfbcce401d1af237b4fbdf9b04a37433ae3efdd
Aditya Soni <aditya.soni@intel.com>
2025-05-29 14:53:55 +0530

    Added S3 in metadata schema

--- microservices/dlstreamer-pipeline-server/src/publisher/influx/influx_schema.py
+++ microservices/dlstreamer-pipeline-server/src/publisher/influx/influx_schema.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, post_dump, INCLUDE
+from marshmallow import Schema, fields, post_dump, EXCLUDE
 import json
 
 class DataSchema(Schema):
@@ -21,13 +21,14 @@ class DataSchema(Schema):
     timestamp = fields.Int(load_default=None)
     tags = fields.Dict(load_default={})
     time = fields.Int(required=True)
+    S3_meta = fields.Dict(required=False)
     
     class Meta:
-        unknown = INCLUDE
+        unknown = EXCLUDE  # To ignore unknown fields, the fields not defined in the schema
 
     @post_dump
     def stringify_complex(self, data, **kwargs):
-        for key in ["objects", "resolution", "pipeline", "gva_meta", "tags"]:
+        for key in ["objects", "resolution", "pipeline", "gva_meta", "tags","S3_meta"]:
             if key in data:
                 data[key] = json.dumps(data[key])
         return data
