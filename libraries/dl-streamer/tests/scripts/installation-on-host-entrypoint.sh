983300f94d7583a7dfe7c6e70099afaafd274db5
dmichalo <dawid.michalowski@intel.com>
2025-06-10 14:59:57 +0200

    Add installing libcairo2-dev (without sudo)
    Signed-off-by: dmichalo <dawid.michalowski@intel.com>

--- libraries/dl-streamer/tests/scripts/installation-on-host-entrypoint.sh
+++ libraries/dl-streamer/tests/scripts/installation-on-host-entrypoint.sh
@@ -95,100 +95,100 @@ chmod +x $PREREQUISITES_SCRIPT_PATH/DLS_install_prerequisites.sh
 $PREREQUISITES_SCRIPT_PATH/DLS_install_prerequisites.sh --reinstall-npu-driver=no
 
 # Configure repositories before installation
-# echo_color "Starting to configure OpenVINO™ repository access before DL Streamer installation" "blue"
-# sudo -E wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+echo_color "Starting to configure OpenVINO™ repository access before DL Streamer installation" "blue"
+sudo -E wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
 
-# . /etc/os-release
-# if [[ ! " jammy noble " =~  ${VERSION_CODENAME}  ]]; then
-#     echo_color "Ubuntu version ${VERSION_CODENAME} not supported" "red"
-# else
-#     if [[ "${VERSION_CODENAME}" == "jammy" ]]; then
-#         sudo bash -c 'echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/openvino/2025 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2025.list'
-#         echo_color "Completed to configure OpenVINO™ repository access before DL Streamer installation for Ubuntu22" "magenta"
-#     fi
-#     if [[ "${VERSION_CODENAME}" == "noble" ]]; then
-#         sudo bash -c 'echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/openvino/2025 ubuntu24 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2025.list'
-#         echo_color "Completed to configure OpenVINO™ repository access before DL Streamer installation for Ubuntu24" "magenta"
-#     fi
-# fi
+. /etc/os-release
+if [[ ! " jammy noble " =~  ${VERSION_CODENAME}  ]]; then
+    echo_color "Ubuntu version ${VERSION_CODENAME} not supported" "red"
+else
+    if [[ "${VERSION_CODENAME}" == "jammy" ]]; then
+        sudo bash -c 'echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/openvino/2025 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2025.list'
+        echo_color "Completed to configure OpenVINO™ repository access before DL Streamer installation for Ubuntu22" "magenta"
+    fi
+    if [[ "${VERSION_CODENAME}" == "noble" ]]; then
+        sudo bash -c 'echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/openvino/2025 ubuntu24 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2025.list'
+        echo_color "Completed to configure OpenVINO™ repository access before DL Streamer installation for Ubuntu24" "magenta"
+    fi
+fi
 
-# echo_color "Executing: sudo apt update" "blue"
-# sudo apt update
-# echo_color  "Completed: sudo apt update" "magenta"
+echo_color "Executing: sudo apt update" "blue"
+sudo apt update
+echo_color  "Completed: sudo apt update" "magenta"
 
-# #DLStreamer installation
-# echo_color "Executing: sudo apt update" "blue"
-# sudo apt-get update
-# echo_color  "Completed: sudo apt update" "magenta"
+#DLStreamer installation
+echo_color "Executing: sudo apt update" "blue"
+sudo apt-get update
+echo_color  "Completed: sudo apt update" "magenta"
 
-# echo_color "Executing: sudo apt install -y ./intel-dlstreamer*" "blue"
-# cd $DEB_PKGS_PATH
-# sudo apt install -y ./intel-dlstreamer*
-# echo_color "Completed: sudo apt install -y ./intel-dlstreamer*" "magenta"
+echo_color "Executing: sudo apt install -y ./intel-dlstreamer*" "blue"
+cd $DEB_PKGS_PATH
+sudo apt install -y ./intel-dlstreamer*
+echo_color "Completed: sudo apt install -y ./intel-dlstreamer*" "magenta"
 
-# #unset env variables
-# echo_color "Unsetting environment variables" "blue"
-# unset LIBVA_DRIVER_NAME
-# unset GST_PLUGIN_PATH
-# unset LD_LIBRARY_PATH
-# unset LIBVA_DRIVERS_PATH
-# unset GST_VA_ALL_DRIVERS
-# unset MODEL_PROC_PATH
-# unset PYTHONPATH
-# unset TERM
-# unset GST_VAAPI_DRM_DEVICE
-# unset GST_VAAPI_ALL_DRIVERS
+#unset env variables
+echo_color "Unsetting environment variables" "blue"
+unset LIBVA_DRIVER_NAME
+unset GST_PLUGIN_PATH
+unset LD_LIBRARY_PATH
+unset LIBVA_DRIVERS_PATH
+unset GST_VA_ALL_DRIVERS
+unset MODEL_PROC_PATH
+unset PYTHONPATH
+unset TERM
+unset GST_VAAPI_DRM_DEVICE
+unset GST_VAAPI_ALL_DRIVERS
 
-# # Display the values of the environment variables
-# echo_color "Displaying the values of the environment variables:" "blue"
-# echo "LIBVA_DRIVER_NAME: ${LIBVA_DRIVER_NAME}"
-# echo "GST_PLUGIN_PATH: ${GST_PLUGIN_PATH}"
-# echo "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
-# echo "LIBVA_DRIVERS_PATH: ${LIBVA_DRIVERS_PATH}"
-# echo "GST_VA_ALL_DRIVERS: ${GST_VA_ALL_DRIVERS}"
-# echo "MODEL_PROC_PATH: ${MODEL_PROC_PATH}"
-# echo "PYTHONPATH: ${PYTHONPATH}"
-# echo "TERM: ${TERM}"
-# echo "GST_VAAPI_DRM_DEVICE: ${GST_VAAPI_DRM_DEVICE}"
-# echo "GST_VAAPI_ALL_DRIVERS: ${GST_VAAPI_ALL_DRIVERS}"
+# Display the values of the environment variables
+echo_color "Displaying the values of the environment variables:" "blue"
+echo "LIBVA_DRIVER_NAME: ${LIBVA_DRIVER_NAME}"
+echo "GST_PLUGIN_PATH: ${GST_PLUGIN_PATH}"
+echo "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
+echo "LIBVA_DRIVERS_PATH: ${LIBVA_DRIVERS_PATH}"
+echo "GST_VA_ALL_DRIVERS: ${GST_VA_ALL_DRIVERS}"
+echo "MODEL_PROC_PATH: ${MODEL_PROC_PATH}"
+echo "PYTHONPATH: ${PYTHONPATH}"
+echo "TERM: ${TERM}"
+echo "GST_VAAPI_DRM_DEVICE: ${GST_VAAPI_DRM_DEVICE}"
+echo "GST_VAAPI_ALL_DRIVERS: ${GST_VAAPI_ALL_DRIVERS}"
 
-# # set environment variables
-# echo_color "Setting the environment variables" "blue"
-# export LIBVA_DRIVER_NAME=iHD
-# export GST_PLUGIN_PATH=/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/intel/dlstreamer/gstreamer/lib/gstreamer-1.0:/opt/intel/dlstreamer/gstreamer/lib/
-# export LD_LIBRARY_PATH=/opt/intel/dlstreamer/gstreamer/lib:/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/intel/dlstreamer/lib/gstreamer-1.0:/usr/lib:/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/opencv:/opt/openh264:/opt/rdkafka:/opt/ffmpeg:/usr/local/lib/gstreamer-1.0:/usr/local/lib
-# export LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri
-# export GST_VA_ALL_DRIVERS=1
-# export PATH=/python3venv/bin:/opt/intel/dlstreamer/gstreamer/bin:/opt/intel/dlstreamer/build/intel64/Release/bin:$PATH
-# export PYTHONPATH=/opt/intel/dlstreamer/gstreamer/lib/python3/dist-packages:/home/dlstreamer/dlstreamer/python:/opt/intel/dlstreamer/gstreamer/lib/python3/dist-packages:
-# export TERM=xterm
+# set environment variables
+echo_color "Setting the environment variables" "blue"
+export LIBVA_DRIVER_NAME=iHD
+export GST_PLUGIN_PATH=/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/intel/dlstreamer/gstreamer/lib/gstreamer-1.0:/opt/intel/dlstreamer/gstreamer/lib/
+export LD_LIBRARY_PATH=/opt/intel/dlstreamer/gstreamer/lib:/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/intel/dlstreamer/lib/gstreamer-1.0:/usr/lib:/opt/intel/dlstreamer/build/intel64/Release/lib:/opt/opencv:/opt/openh264:/opt/rdkafka:/opt/ffmpeg:/usr/local/lib/gstreamer-1.0:/usr/local/lib
+export LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri
+export GST_VA_ALL_DRIVERS=1
+export PATH=/python3venv/bin:/opt/intel/dlstreamer/gstreamer/bin:/opt/intel/dlstreamer/build/intel64/Release/bin:$PATH
+export PYTHONPATH=/opt/intel/dlstreamer/gstreamer/lib/python3/dist-packages:/home/dlstreamer/dlstreamer/python:/opt/intel/dlstreamer/gstreamer/lib/python3/dist-packages:
+export TERM=xterm
 
-# # Display the values of the environment variables
-# echo_color "Displaying the values of the environment variables:" "blue"
-# echo "LIBVA_DRIVER_NAME: ${LIBVA_DRIVER_NAME}"
-# echo "GST_PLUGIN_PATH: ${GST_PLUGIN_PATH}"
-# echo "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
-# echo "LIBVA_DRIVERS_PATH: ${LIBVA_DRIVERS_PATH}"
-# echo "GST_VA_ALL_DRIVERS: ${GST_VA_ALL_DRIVERS}"
-# echo "MODEL_PROC_PATH: ${MODEL_PROC_PATH}"
-# echo "PYTHONPATH: ${PYTHONPATH}"
-# echo "TERM: ${TERM}"
-# echo "GST_VAAPI_DRM_DEVICE: ${GST_VAAPI_DRM_DEVICE}"
-# echo "GST_VAAPI_ALL_DRIVERS: ${GST_VAAPI_ALL_DRIVERS}"
+# Display the values of the environment variables
+echo_color "Displaying the values of the environment variables:" "blue"
+echo "LIBVA_DRIVER_NAME: ${LIBVA_DRIVER_NAME}"
+echo "GST_PLUGIN_PATH: ${GST_PLUGIN_PATH}"
+echo "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
+echo "LIBVA_DRIVERS_PATH: ${LIBVA_DRIVERS_PATH}"
+echo "GST_VA_ALL_DRIVERS: ${GST_VA_ALL_DRIVERS}"
+echo "MODEL_PROC_PATH: ${MODEL_PROC_PATH}"
+echo "PYTHONPATH: ${PYTHONPATH}"
+echo "TERM: ${TERM}"
+echo "GST_VAAPI_DRM_DEVICE: ${GST_VAAPI_DRM_DEVICE}"
+echo "GST_VAAPI_ALL_DRIVERS: ${GST_VAAPI_ALL_DRIVERS}"
 
-# #Change owner of /opt/opencv (needed for cpp_draw_attributes sample)
-# echo_color "Show /opt directory before chown command" "blue"
-# ls -l /opt
-# sudo chown -R $USER /opt/opencv
-# echo_color "Show /opt directory after chown command" "blue"
-# ls -l /opt
+#Change owner of /opt/opencv (needed for cpp_draw_attributes sample)
+echo_color "Show /opt directory before chown command" "blue"
+ls -l /opt
+sudo chown -R $USER /opt/opencv
+echo_color "Show /opt directory after chown command" "blue"
+ls -l /opt
 
-# echo "Checking if vapostproc exists"
-# gst-inspect-1.0 vapostproc
+echo "Checking if vapostproc exists"
+gst-inspect-1.0 vapostproc
 
-# if gst-inspect-1.0 gvadetect &> /dev/null; then
-#     echo_color " Intel® DL Streamer verification successful" "green"
-# else
-#     handle_error " Intel® DL Streamer verification failed"
-#     exit 1
-# fi
+if gst-inspect-1.0 gvadetect &> /dev/null; then
+    echo_color " Intel® DL Streamer verification successful" "green"
+else
+    handle_error " Intel® DL Streamer verification failed"
+    exit 1
+fi
