d655f1be8be0f6ed597a0d3846a9eb30906d4c6a
nszczygl9 <118973656+nszczygl9@users.noreply.github.com>
2025-06-16 13:49:16 +0200

    .

--- microservices/dlstreamer-pipeline-server/plugins/gst-udf-loader/tests/gst_appsink.py
+++ microservices/dlstreamer-pipeline-server/plugins/gst-udf-loader/tests/gst_appsink.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2022 Intel Corporationn
+# Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: MIT
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
