375ec33018e80b5972bee2481ca1bf38c3d6074f
dmichalo <dawid.michalowski@intel.com>
2025-06-24 10:04:32 +0200

    Add linkcheck_ignore for Intel links
    Signed-off-by: dmichalo <dawid.michalowski@intel.com>

--- libraries/dl-streamer/docs/source/conf.py
+++ libraries/dl-streamer/docs/source/conf.py
@@ -81,13 +81,6 @@ spelling_show_suggestions=True
 
 # Enable anchors check
 linkcheck_anchors=False
-
-linkcheck_request_headers = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/115.0.0.0 Safari/537.36"
-    ),
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-}
+linkcheck_ignore = [
+    r'https://www.intel.com/.*'
+]
