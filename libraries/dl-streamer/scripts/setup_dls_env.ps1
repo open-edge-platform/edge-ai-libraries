d74d253f66c4e230f64610b12aee25481d025206
Marcin Wadolkowski <marcin.wadolkowski@intel.com>
2025-07-14 08:26:01 +0200

    [DLStreamer] fixed needed for Windows CI

--- libraries/dl-streamer/scripts/setup_dls_env.ps1
+++ libraries/dl-streamer/scripts/setup_dls_env.ps1
@@ -26,7 +26,7 @@ if (-Not [Environment]::GetEnvironmentVariable('LIBVA_DRIVER_NAME', 'User')) {
 
 if (-Not [Environment]::GetEnvironmentVariable('GST_PLUGIN_PATH', 'User')) {
     $CURRENT_DIR = (Get-Item .).FullName
-	[Environment]::SetEnvironmentVariable('GST_PLUGIN_PATH', $CURRENT_DIR, [System.EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('GST_PLUGIN_PATH', "C:\gstreamer\1.0\msvc_x86_64\bin;C:\gstreamer\1.0\msvc_x86_64\lib\gstreamer-1.0;$CURRENT_DIR", [System.EnvironmentVariableTarget]::User)
 }
 
 if (-Not [Environment]::GetEnvironmentVariable('GST_PLUGIN_SCANNER', 'User')) {
@@ -39,7 +39,9 @@ if (-Not [Environment]::GetEnvironmentVariable('Path', 'User').Contains('gstream
 	echo 'Searching for gst-launch-1.0.exe'
 	$GSTREAMER_DIR = (Get-ChildItem -Filter gst-launch-1.0.exe -Recurse -Path 'C:\gstreamer' -Include 'gst-launch-1.0.exe' -ErrorAction SilentlyContinue).DirectoryName | Select-Object -First 1
 	$USER_PATH = [Environment]::GetEnvironmentVariable('Path', 'User')
-	[Environment]::SetEnvironmentVariable('Path', $USER_PATH + ';' + $GSTREAMER_DIR, [System.EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('OpenVINO_DIR', "C:\openvino\runtime\cmake", [System.EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('OPENVINO_LIB_PATHS', "C:\openvino\runtime\3rdparty\tbb\bin;C:\openvino\runtime\bin\intel64\Release", [System.EnvironmentVariableTarget]::User)
+	[Environment]::SetEnvironmentVariable('Path', $USER_PATH + ';' + $GSTREAMER_DIR + ";C:\openvino\runtime\3rdparty\tbb\bin;C:\openvino\runtime\bin\intel64\Release", [System.EnvironmentVariableTarget]::User)
 }
 
 $env:Path = [System.Environment]::GetEnvironmentVariable('Path','Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path','User')
@@ -58,7 +60,6 @@ if (-Not [System.IO.File]::Exists('C:\openvino\setupvars.ps1')) {
 	echo 'OpenVINO found'
 }
 
-& C:\openvino\setupvars.ps1
 
 echo ""
 echo "Generating GStreamer cache. It may take up to a few minutes for the first run"
