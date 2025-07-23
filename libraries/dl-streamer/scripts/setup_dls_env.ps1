dccd2793f6abd4a22ae7fb695580e8970ad684b5
dmichalo <dawid.michalowski@intel.com>
2025-07-23 21:23:58 +0200

    Modify setup_dls_env.ps1
    Signed-off-by: dmichalo <dawid.michalowski@intel.com>

--- libraries/dl-streamer/scripts/setup_dls_env.ps1
+++ libraries/dl-streamer/scripts/setup_dls_env.ps1
@@ -8,51 +8,30 @@ if (-Not (Test-Path 'C:\gstreamer')) {
 	echo 'Please install GStreamer to folder C:\gstreamer and then run the script again.'
 	echo 'Link: https://gstreamer.freedesktop.org/data/pkg/windows/1.26.2/msvc/gstreamer-1.0-msvc-x86_64-1.26.2.msi'
 	exit
+} else {
+	echo 'GStreamer found in folder C:\gstreamer'
 }
 
-if (-Not [Environment]::GetEnvironmentVariable('LIBVA_DRIVER_NAME', 'User')) {
-	$NUGET_PATH = (Get-Item .).FullName + '/nuget.exe'
-	if (-Not [System.IO.File]::Exists($NUGET_PATH)) {
-        curl -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe
-		Start-Process -NoNewWindow -FilePath $NUGET_PATH -ArgumentList 'install', 'Microsoft.Direct3D.VideoAccelerationCompatibilityPack'
-	}
-	[Environment]::SetEnvironmentVariable('LIBVA_DRIVER_NAME', 'vaon12', [System.EnvironmentVariableTarget]::User)
-	[Environment]::SetEnvironmentVariable('LIBVA_DRIVERS_PATH', (Get-Item .).FullName + '\Microsoft.Direct3D.VideoAccelerationCompatibilityPack.1.0.2\build\native\x64\bin\', [System.EnvironmentVariableTarget]::User)
-    $USER_PATH = [Environment]::GetEnvironmentVariable('Path', 'User')
-	if (-Not $USER_PATH.Contains('VideoAccelerationCompatibilityPack')) {
-		[Environment]::SetEnvironmentVariable('Path', $USER_PATH + ';' + [Environment]::GetEnvironmentVariable('LIBVA_DRIVERS_PATH', 'User'), [System.EnvironmentVariableTarget]::User)
-	}
+echo 'Setting variables: LIBVA_DRIVER_NAME, LIBVA_DRIVERS_PATH, Path (for LIBVA)'
+[Environment]::SetEnvironmentVariable('LIBVA_DRIVER_NAME', 'vaon12', [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable('LIBVA_DRIVERS_PATH', (Get-Item .).FullName + '\Microsoft.Direct3D.VideoAccelerationCompatibilityPack.1.0.2\build\native\x64\bin\', [System.EnvironmentVariableTarget]::User)
+$USER_PATH = [Environment]::GetEnvironmentVariable('Path', 'User')
+if (-Not $USER_PATH.Contains('VideoAccelerationCompatibilityPack')) {
+	[Environment]::SetEnvironmentVariable('Path', $USER_PATH + ';' + [Environment]::GetEnvironmentVariable('LIBVA_DRIVERS_PATH', 'User'), [System.EnvironmentVariableTarget]::User)
 }
 
-if (-Not [Environment]::GetEnvironmentVariable('GST_PLUGIN_PATH', 'User')) {
-    $CURRENT_DIR = (Get-Item .).FullName
-	[Environment]::SetEnvironmentVariable('GST_PLUGIN_PATH', "C:\gstreamer\1.0\msvc_x86_64\bin;C:\gstreamer\1.0\msvc_x86_64\lib\gstreamer-1.0;$CURRENT_DIR", [System.EnvironmentVariableTarget]::User)
-}
+echo 'Setting variables: GST_PLUGIN_PATH'
+$CURRENT_DIR = (Get-Item .).FullName
+[Environment]::SetEnvironmentVariable('GST_PLUGIN_PATH', "C:\gstreamer\1.0\msvc_x86_64\bin;C:\gstreamer\1.0\msvc_x86_64\lib\gstreamer-1.0;$CURRENT_DIR", [System.EnvironmentVariableTarget]::User)
 
-if (-Not [Environment]::GetEnvironmentVariable('GST_PLUGIN_SCANNER', 'User')) {
-	echo 'Searching for gst-plugin-scanner.exe'
-	$GSTREAMER_PLUGIN_SCANNER_PATH = (Get-ChildItem -Filter gst-plugin-scanner.exe -Recurse -Path 'C:\gstreamer' -Include 'gst-plugin-scanner.exe' -ErrorAction SilentlyContinue) | Select-Object -First 1
-	[Environment]::SetEnvironmentVariable('GST_PLUGIN_SCANNER', $GSTREAMER_PLUGIN_SCANNER_PATH, [System.EnvironmentVariableTarget]::User)
-}
+echo 'Setting variables: GST_PLUGIN_SCANNER'
+$GSTREAMER_PLUGIN_SCANNER_PATH = (Get-ChildItem -Filter gst-plugin-scanner.exe -Recurse -Path 'C:\gstreamer' -Include 'gst-plugin-scanner.exe' -ErrorAction SilentlyContinue) | Select-Object -First 1
+[Environment]::SetEnvironmentVariable('GST_PLUGIN_SCANNER', $GSTREAMER_PLUGIN_SCANNER_PATH, [System.EnvironmentVariableTarget]::User)
 
-if (-Not [Environment]::GetEnvironmentVariable('Path', 'User').Contains('gstreamer')) {
-	echo 'Searching for gst-launch-1.0.exe'
-	$GSTREAMER_DIR = (Get-ChildItem -Filter gst-launch-1.0.exe -Recurse -Path 'C:\gstreamer' -Include 'gst-launch-1.0.exe' -ErrorAction SilentlyContinue).DirectoryName | Select-Object -First 1
-	$USER_PATH = [Environment]::GetEnvironmentVariable('Path', 'User')
-	[Environment]::SetEnvironmentVariable('Path', $USER_PATH + ';' + $GSTREAMER_DIR, [System.EnvironmentVariableTarget]::User)
-}
-
-if (-Not [Environment]::GetEnvironmentVariable('Path', 'User').Contains('openvino')) {
-	$USER_PATH = [Environment]::GetEnvironmentVariable('Path', 'User')
-	[Environment]::SetEnvironmentVariable('OpenVINO_DIR', "C:\openvino\runtime\cmake", [System.EnvironmentVariableTarget]::User)
-	[Environment]::SetEnvironmentVariable('OPENVINO_LIB_PATHS', "C:\openvino\runtime\3rdparty\tbb\bin;C:\openvino\runtime\bin\intel64\Release", [System.EnvironmentVariableTarget]::User)
-	[Environment]::SetEnvironmentVariable('Path', $USER_PATH + ";C:\openvino\runtime\3rdparty\tbb\bin;C:\openvino\runtime\bin\intel64\Release", [System.EnvironmentVariableTarget]::User)
-}
-
-$env:Path = [System.Environment]::GetEnvironmentVariable('Path','Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path','User')
-
-$env:GST_PLUGIN_PATH = [System.Environment]::GetEnvironmentVariable('GST_PLUGIN_PATH','User')
-$env:GST_PLUGIN_SCANNER = [System.Environment]::GetEnvironmentVariable('GST_PLUGIN_SCANNER','User')
+echo 'Setting variables: Path (for gst-launch-1.0)'
+$GSTREAMER_DIR = (Get-ChildItem -Filter gst-launch-1.0.exe -Recurse -Path 'C:\gstreamer' -Include 'gst-launch-1.0.exe' -ErrorAction SilentlyContinue).DirectoryName | Select-Object -First 1
+$USER_PATH = [Environment]::GetEnvironmentVariable('Path', 'User')
+[Environment]::SetEnvironmentVariable('Path', $USER_PATH + ';' + $GSTREAMER_DIR, [System.EnvironmentVariableTarget]::User)
 
 echo 'Searching for OpenVINO'
 if (-Not [System.IO.File]::Exists('C:\openvino\setupvars.ps1')) {
@@ -65,6 +44,19 @@ if (-Not [System.IO.File]::Exists('C:\openvino\setupvars.ps1')) {
 	echo 'OpenVINO found'
 }
 
+echo 'Setting variables:, OpenVINO_DIR, OPENVINO_LIB_PATHS, Path (for OpenVINO)'
+$USER_PATH = [Environment]::GetEnvironmentVariable('Path', 'User')
+[Environment]::SetEnvironmentVariable('OpenVINO_DIR', "C:\openvino\runtime\cmake", [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable('OPENVINO_LIB_PATHS', "C:\openvino\runtime\3rdparty\tbb\bin;C:\openvino\runtime\bin\intel64\Release", [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable('Path', $USER_PATH + ";C:\openvino\runtime\3rdparty\tbb\bin;C:\openvino\runtime\bin\intel64\Release", [System.EnvironmentVariableTarget]::User)
+
+$env:Path = [System.Environment]::GetEnvironmentVariable('Path','Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path','User')
+
+$env:GST_PLUGIN_PATH = [System.Environment]::GetEnvironmentVariable('GST_PLUGIN_PATH','User')
+$env:GST_PLUGIN_SCANNER = [System.Environment]::GetEnvironmentVariable('GST_PLUGIN_SCANNER','User')
+
+echo "User environment variables:"
+Get-ChildItem Env:
 
 echo ""
 echo "Generating GStreamer cache. It may take up to a few minutes for the first run"
