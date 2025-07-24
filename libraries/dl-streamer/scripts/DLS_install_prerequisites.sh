13277e732338cef4c1b4dec1925633e94eaaa700
nszczygl9 <118973656+nszczygl9@users.noreply.github.com>
2025-07-24 14:48:27 +0200

    Change npu driver version in script

--- libraries/dl-streamer/scripts/DLS_install_prerequisites.sh
+++ libraries/dl-streamer/scripts/DLS_install_prerequisites.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: MIT
 # ==============================================================================
 
-npu_driver_version='1.13.0'
+npu_driver_version='1.19.0'
 reinstall_npu_driver='no'  # Default value for reinstalling the NPU driver
 on_host_or_docker='host'
 
