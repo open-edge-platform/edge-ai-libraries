a1bc67b88f4ae011069679f529fa983d492d5ea3
nszczygl9 <118973656+nszczygl9@users.noreply.github.com>
2025-08-07 10:34:33 +0200

    modify entrypoint.sh script

--- .github/actions/common/code-style/entrypoint.sh
+++ .github/actions/common/code-style/entrypoint.sh
@@ -8,32 +8,27 @@
 set -e
 
 SOURCE="${1:-/sources}"
-
 SOURCE=$(realpath --relative-to=. "$SOURCE" | sed 's:/*$::')
 
-mkdir -p /styled
-mkdir -p "/styled/$(dirname "$SOURCE")"
-cp -R "$SOURCE" "/styled/$SOURCE"
+STYLED_DIR="./_styled"
+OUTPUT_DIR="./_output"
+mkdir -p "$STYLED_DIR" "$OUTPUT_DIR"
 
-find "/styled/$SOURCE" \
-  \( -name '*.c' \
-  -o -name '*.cc' \
-  -o -name '*.cpp' \
-  -o -name '*.h' \
-  -o -name '*.hh' \
-  -o -name '*.hpp' \) \
+mkdir -p "$(dirname "$STYLED_DIR/$SOURCE")"
+cp -R "$SOURCE" "$STYLED_DIR/$SOURCE"
+
+find "$STYLED_DIR/$SOURCE" \
+  \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hh' -o -name '*.hpp' \) \
   -exec sh -c "clang-format style=file -i '{}' 2>&1 | sed '/No such file or directory/d'" \;
 
-output=$(diff -u --recursive "$SOURCE" "/styled/$SOURCE" || true)
+output=$(diff -u --recursive "$SOURCE" "$STYLED_DIR/$SOURCE" || true)
 
 if [[ -n "$output" ]]; then
-    mkdir -p /output
-    diff2html -F /output/diff.html -d word -s "side" -i stdin <<< "$output"
-    sed -i '37d;38i<h1>Code style diff</h1>' /output/diff.html
+    diff2html -F "$OUTPUT_DIR/diff.html" -d word -s "side" -i stdin <<< "$output"
+    sed -i '37d;38i<h1>Code style diff</h1>' "$OUTPUT_DIR/diff.html"
     echo "❌ There are problems with code styles"
     exit 1
 else
     echo "✅ Code styles are fine"
     exit 0
 fi
-
