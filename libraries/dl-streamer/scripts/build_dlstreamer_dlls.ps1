eee64aad24dd1e87ee7cf7f7b4a3cf92dc66d298
Michalowski, Dawid <dawid.michalowski@intel.com>
2025-09-12 09:54:53 +0200

    Fix typo in Write-Host
    Signed-off-by: Michalowski, Dawid <dawid.michalowski@intel.com>

--- libraries/dl-streamer/scripts/build_dlstreamer_dlls.ps1
+++ libraries/dl-streamer/scripts/build_dlstreamer_dlls.ps1
@@ -30,7 +30,7 @@ if (-Not (Test-Path "C:\\BuildTools")) {
 	Write-Host "####################################### Installing VS BuildTools #######################################"
 	Invoke-WebRequest -OutFile $DLSTREAMER_TMP\\vs_buildtools.exe -Uri https://aka.ms/vs/17/release/vs_buildtools.exe
 	Start-Process -Wait -FilePath $DLSTREAMER_TMP\vs_buildtools.exe -ArgumentList "--quiet", "--wait", "--norestart", "--nocache", "--installPath", "C:\\BuildTools", "--add", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64", "--add", "Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core"
-	rite-Host "####################################### Installing VS BuildTools #######################################"
+	Write-Host "################################################ Done ##################################################"
 } else {
 	Write-Host "################################# VS BuildTools already installed #####################################"
 }
