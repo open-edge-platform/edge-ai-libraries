3f8604024a81b721defc56e4dfd0cacc37b44c1c
Michalowski, Dawid <dawid.michalowski@intel.com>
2025-10-21 10:04:27 +0200

    Fix typo
    Signed-off-by: Michalowski, Dawid <dawid.michalowski@intel.com>

--- libraries/dl-streamer/samples/download_public_models.sh
+++ libraries/dl-streamer/samples/download_public_models.sh
@@ -235,7 +235,7 @@ fi
 
 # Install dependencies for CLIP models
 if [[ "${MODEL:-}" =~ clip.* || "${MODEL:-}" == "all" ]]; then
-  pip install --no-cache-dir --upgrade torch==2.8.0 torchaudio=2.8.0 torchvision==0.23.0 || handle_error $LINENO
+  pip install --no-cache-dir --upgrade torch==2.8.0 torchaudio==2.8.0 torchvision==0.23.0 || handle_error $LINENO
   pip install --no-cache-dir transformers || handle_error $LINENO
   pip install --no-cache-dir pillow || handle_error $LINENO
 fi
@@ -489,7 +489,7 @@ done
 REPO_DIR="$MODELS_PATH/yolov5_repo"
 if [ "$MODEL_IN_LISTv5" = true ] && [ ! -d "$REPO_DIR" ]; then
   git clone https://github.com/ultralytics/yolov5 "$REPO_DIR"
-  pip install --no-cache-dir --index-url https://download.pytorch.org/whl/cpu torch==2.8.0 torchaudio=2.8.0 torchvision==0.23.0
+  pip install --no-cache-dir --index-url https://download.pytorch.org/whl/cpu torch==2.8.0 torchaudio==2.8.0 torchvision==0.23.0
   pip install --no-cache-dir -r "$REPO_DIR"/requirements.txt
 fi
 
@@ -582,7 +582,7 @@ if [ "$MODEL" == "yolov7" ] || [ "$MODEL" == "yolo_all" ] || [ "$MODEL" == "all"
     mv yolov7.bin "$MODEL_DIR/FP32"
     cd ..
     rm -rf yolov7
-    pip install --no-cache-dir --upgrade torch==2.8.0 torchaudio=2.8.0 torchvision==0.23.0 || handle_error $LINENO
+    pip install --no-cache-dir --upgrade torch==2.8.0 torchaudio==2.8.0 torchvision==0.23.0 || handle_error $LINENO
   else
     echo_color "\nModel already exists: $MODEL_DIR.\n" "yellow"
   fi
