baf3570d2c38e9a51ac8c7d1fe45a44683f7390e
nszczygl9 <118973656+nszczygl9@users.noreply.github.com>
2025-11-14 16:12:13 +0100

    use the same apporach in script as in dockerfile

--- libraries/dl-streamer/scripts/DLS_install_prerequisites.sh
+++ libraries/dl-streamer/scripts/DLS_install_prerequisites.sh
@@ -341,7 +341,9 @@ setup_gpu(){
         $SUDO_PREFIX apt update
         echo "Snapshot: 20250911T030400Z" | $SUDO_PREFIX tee -a /etc/apt/sources.list.d/$INTEL_GPU_LIST
         $SUDO_PREFIX apt update
-        install_packages libze-intel-gpu1 libze1 intel-ocloc intel-opencl-icd clinfo intel-gsc libze-intel-gpu-raytracing intel-media-va-driver-non-free libmfx-gen1 libvpl2 libvpl-tools libva-glx2 va-driver-all vainfo
+        install_packages intel-metrics-discovery intel-gsc libvpl2 \
+            libze-intel-gpu1=25.31.34666.3-1~24.04~ppa2 libze1=1.23.1-1~24.04~ppa1 intel-opencl-icd=25.31.34666.3-1~24.04~ppa2 clinfo=3.0.23.01.25-1build1 \
+            intel-media-va-driver-non-free=25.3.2-0ubuntu1~24.04~ppa1 libmfx-gen1=25.3.1-0ubuntu1~24.04~ppa1 libvpl-tools=1.4.0-0ubuntu1~24.04~ppa1 libva-glx2=2.22.0-1ubuntu1~24.04~ppa1 va-driver-all=2.22.0-1ubuntu1~24.04~ppa1 vainfo=2.22.0-0ubuntu1~24.04~ppa1
     elif [ "$ubuntu_version" == "22.04" ]; then
         configure_repository "$INTEL_CL_GPU_KEY_URL" "$INTEL_GPU_KEYRING_PATH" "$INTEL_CL_GPU_REPO_URL" "$INTEL_GPU_LIST"
         install_packages clinfo libze-intel-gpu1=25.18.33578.15-1146~22.04 libze1=1.21.9.0-1136~22.04 intel-media-va-driver-non-free=25.2.4-1146~22.04 intel-opencl-icd=25.18.33578.15-1146~22.04
