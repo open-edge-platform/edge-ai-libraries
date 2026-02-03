a3bea2f942394c9cadf74b42a10afcdcc884afc0
Dariusz Lemiech <dariusz.lemiech@intel.com>
2026-02-03 13:03:32 +0100

    changed layout to sidebar

--- tools/visual-pipeline-and-platform-evaluation-tool/ui/src/components/ui/sidebar.tsx
+++ tools/visual-pipeline-and-platform-evaluation-tool/ui/src/components/ui/sidebar.tsx
@@ -382,7 +382,7 @@ function SidebarGroup({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="sidebar-group"
       data-sidebar="group"
-      className={cn("relative flex w-full min-w-0 flex-col py-2", className)}
+      className={cn("relative flex w-full min-w-0 flex-col pb-2", className)}
       {...props}
     />
   );
