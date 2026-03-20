35c32665ff87f04d6d3bf7eb53c67b6e1e6ade25
ktaube26 <krystian.taube@intel.com>
2026-03-20 07:06:21 -0700

    Set version to 2026.0.0

--- tools/visual-pipeline-and-platform-evaluation-tool/setup_env.sh
+++ tools/visual-pipeline-and-platform-evaluation-tool/setup_env.sh
@@ -3,7 +3,7 @@
 # This script automatically detects the available device (NPU, GPU, or CPU)
 # and writes COMPOSE_PROFILES, RENDER_GROUP_ID, and DOCKER_TAG to the .env file.
 
-VERSION=2026.0.0-20260210-weekly # TODO: Update version when 2026.0.0 is released
+VERSION=2026.0.0
 COMPOSE_PROFILES=""
 RENDER_GROUP_ID=""
 
